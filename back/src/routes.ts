cc871b0d175abf75dd1f0b76c4d5ede0cefb4be1
lucasttavares <zlucas.np@gmail.com>
2023-07-08 20:19:02 -0300

    Mudança de nome da pasta

--- back/src/routes.ts
+++
@@ -1,9 +0,0 @@
-import express, { request, response } from "express";
-
-const routes = express.Router();
-
-routes.get('/cards', (request, response) =>{
-    return response.json({ message: 'olá olá'})
-})  
-
-export default routes
