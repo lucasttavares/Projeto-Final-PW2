45ad9dc16002523fbc15ac975416a0fccfd7efa2
Rafael Barroso <rafashpb@gmail.com>
2023-07-12 18:39:20 -0300

    Tela de Login

--- front-end/src/pages/Login/index.tsx
+++ front-end/src/pages/Login/index.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
-  return <h1>Login</h1>;
-};
+import api from '../../services/api';
+import "./style.css";
+
+function Login() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
+  async function login() {
+    const response = await api.get("admin", {
+      params: {
+        username,
+        password
+      },
+    });
+
+  }
+  return (
+    <div className="login-form-container">
+      <form onSubmit={login} className="login-form">
+        <div className="input-group">
+          <label>Username:</label>
+          <input type="text" value={username} onChange={(e) => {setUsername(e.target.value);}} />
+        </div>
+        <div>
+          <label>Senha:</label>
+          <input type="password" value={password} onChange={(e) => {setPassword(e.target.value);}} />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
+}
 
 export default Login;
