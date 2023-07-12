84405d6227162557109c3244d15027ef987ad04a
lucasttavares <zlucas.np@gmail.com>
2023-07-12 11:50:59 -0300

    Ajustando banco de dados

--- back-end/src/database/migrations/01_create_users.ts
+++ back-end/src/database/migrations/01_create_users.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex) {
     return knex.schema.createTable('users', table => {
       table.increments('id').primary();
       table.string('name').notNullable();
-      table.string('avatar').notNullable();
+      table.string('avatar').nullable();
       table.string('numero').notNullable();
       table.string('bio').notNullable();
     });
--- back-end/src/database/migrations/02_create_card.ts
+++ back-end/src/database/migrations/02_create_card.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex) {
     return knex.schema.createTable('cards', table => {
       table.increments('id').primary();
       table.string('subject').notNullable();
-      table.string('avatar').notNullable();
+      table.string('avatar').nullable();
 
       table.integer('user_id')
       .notNullable()
