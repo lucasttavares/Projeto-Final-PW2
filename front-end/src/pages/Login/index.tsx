34cf5f9f0feb6cfb484e5cf1aebcd459df010637
lucasttavares <zlucas.np@gmail.com>
2023-07-12 23:35:06 -0300

    Testes

--- front-end/src/pages/Login/index.tsx
+++ front-end/src/pages/Login/index.tsx
@@ -1,28 +1,26 @@
-import React, { useState, FormEvent} from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-
-import api from '../../services/api';
+import api from "../../services/api";
 import "./style.css";
 
 function Login() {
   const navigate = useNavigate();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  
-   
-const handleSubmit = async (event: FormEvent) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
     try {
-      const response = await api.post('/login', {
+      const response = await api.post("/login", {
         username,
         password,
       });
       console.log(response.data);
-      navigate('/')
+      navigate("/list");
     } catch (error) {
-      console.error('Erro de login', error);
+      alert("Credenciais inválidas");
     }
   };
   return (
@@ -30,16 +28,25 @@ const handleSubmit = async (event: FormEvent) => {
       <form onSubmit={handleSubmit} className="login-form">
         <div className="input-group">
           <label>Username:</label>
-          <input type="text" value={username} onChange={(e) => {setUsername(e.target.value);}} />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
         </div>
         <div>
           <label>Senha:</label>
-          <input type="password" value={password} onChange={(e) => {setPassword(e.target.value);}} />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
         </div>
-        <Link to="/list" >
-          <button type="submit">Entrar</button>
-        </Link>
-        
+        <button type="submit">Entrar</button>
       </form>
     </div>
   );
